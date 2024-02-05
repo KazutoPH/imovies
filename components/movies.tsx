93798face0c71cad5cb72f7abeb98a552037c185
KazutoPH <raymartgomez310@gmail.com>
2024-02-05 22:26:05 +0800

    Update movies.tsx

--- components/movies.tsx
+++ components/movies.tsx
@@ -15,28 +15,34 @@ let page = 1
 
 function Movies() {
   const [result, setResult] = useState<any[]>([])
+  const [error, setError] = useState(false)
   const searchParams = useSearchParams()
   const type = searchParams.get('type')
   const query = searchParams.get('query')
   const ref = useRef(null)
   const isInView = useInView(ref)
 
-  let pArray = new Array(10).fill(null).map(( data, i ) => <CardSKeleton key={i}/>)
+  let pArray = new Array(10).fill(null).map((data, i) => <CardSKeleton key={i} />)
 
-  
   useEffect(() => {
     setResult([])
-    const fetchData = async () => {
-      const result = await getMovies(type, query, page);
-      setResult((oldData:any)=>  [...oldData, result])
-      console.log(result)
-    };
 
     if (query)
-      fetchData()
-
+      initialFetchData()
 
   }, [query, type])
+
+  const initialFetchData = async () => {
+    const newRresult = await getMovies(type, query, page);
+    console.log(result)
+
+    if (newRresult.results) {
+      setError(false)
+      setResult((oldData: any) => [...oldData, result])
+    } else {
+      setError(true)
+    }
+  };
 
   // async function onClick() {
   //   page++
@@ -46,21 +52,22 @@ function Movies() {
 
   //     console.log(result)
   //   };
-  
+
   useEffect(() => {
-    const fetchData = async (page:number) => {
-      const Newresult = await getMovies(type, query, page );
-      setResult( (oldData:any)=>  [...oldData, Newresult] )
+    const fetchData = async (page: number) => {
+      const Newresult = await getMovies(type, query, page);
+      if (Newresult.results)
+        setResult((oldData: any) => [...oldData, Newresult])
       // setResult(await result);
 
       console.log(result)
     };
 
-    if(result && isInView) {
+    if (result && isInView) {
       page++
       fetchData(page)
     }
-    
+
   }, [isInView])
 
   // console.log(movies)
@@ -68,33 +75,46 @@ function Movies() {
     <div className='flex flex-col  content-container py-5 gap-10'>
       {/* <div className='gridcontainer w-full'>
         {result ? (
-          result?.results?.map((movie: any, i: any) =>
-          <MovieCard movie={movie} i={i} type={type} key={i}/>
-        )
-        ):(
+          error ? (
+            <p className=" text-white"> There was an Error Loading Movies </p>
+          ) : (
+            <>
+            
+            </>
+
+
+          )
+        )         
+        :(
           pArray.map((skeleton)=>
             skeleton
           )
         )}
       </div> */}
 
-        <div className='relative gridcontainer w-full z-[60]'>
-          {result.length - 1 >= 0 ? (
-            result.map(( data, i ) => 
-              data.results.map((movie:any, index:any) => 
-              <MovieCard movie={movie} i={index} type={type} key={index}/>
+      <div className='relative gridcontainer w-full z-[60]'>
+        {!error ? (
+          <>
+            {result.length - 1 >= 0 ? (
+              result.map((data, i) =>
+                data.results?.map((movie: any, index: any) =>
+                  <MovieCard movie={movie} i={index} type={type} key={index} />
+                )
               )
-            )
-          ):
-          (
-            pArray.map((skeleton)=>
-              skeleton
-            ))
-          }
-        </div>
-      
+            ) :
+              (
+                pArray.map((skeleton) =>
+                  skeleton
+                ))
+            }
+          </>
+        ) : <p className=" text-white">There was an error Loading files</p>
+        }
+
+      </div>
+
       <div ref={ref} className="flex self-center">
-      <LoadingCircle/>
+        <LoadingCircle />
       </div>
 
     </div>
