b4831b3dca8e5082f5c97fd8eef55373accfbcc3
KazutoPH <raymartgomez310@gmail.com>
2024-02-12 17:08:08 +0800

    Margin

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-dark scroll-smooth`}>
+      <body className={`${inter.className} bg-dark scroll-smooth  mt-[85px]`}>
         <Header/>
         {children}
         </body>
