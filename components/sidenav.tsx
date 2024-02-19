c1dab0e00ee3195314082dfacbbecdab300d6853
KazutoPH <raymartgomez310@gmail.com>
2024-02-19 16:04:48 +0800

    Update sidenav.tsx

--- components/sidenav.tsx
+++ components/sidenav.tsx
@@ -1,8 +1,9 @@
-import { navList } from '@/constants/constant'
+import { navList } from '@/constants/constant';
 import { AnimatePresence, motion } from 'framer-motion';
 import Link from 'next/link'
 import React, { Dispatch, SetStateAction, useEffect, useRef, useState } from 'react'
 import { FaX } from 'react-icons/fa6'
+
 
 interface Props {
   setSideNavPress: Dispatch<SetStateAction<boolean>>;
@@ -27,7 +28,7 @@ function SideNav({setSideNavPress, sideNavPress}:Props) {
         transition={{ ease: 'easeInOut', duration: 0.25 }}
         exit={{ x: '100%' }}
       className='absolute right-0 h-full bg-dark'>
-        <div className='flex flex-col px-[5vw] md:px-5'>
+        <div className='flex flex-col px-[5vw] md:px-2'>
           <div
           onClick={ ()=> setSideNavPress(false) }
           className='flex self-end my-[15px] hover:cursor-pointer'>
@@ -37,17 +38,21 @@ function SideNav({setSideNavPress, sideNavPress}:Props) {
             />
           </div>
         
+        <div className='flex flex-col gap-5'>
           {navList.map((nav, i) =>
             <div>
-              <div className='p-2 -my-2 hover:bg-grey ease-in transition rounded'>
-                <p key={i} className='font-semibold text-xl text-white whitespace-nowrap'>{nav.name}</p>
-              </div>
+              <div className='flex flex-row items-center p-2 -my-2 hover:bg-grey ease-in transition rounded'>
+                <div>
+                {nav.icon}
+                </div>  
 
+                <p key={i} className='font-semibold text-xl text-white whitespace-nowrap ml-2'>{nav.name}</p>
+              </div>
               <div>
                 {nav.category.map((category, x) =>
                   <div
                   onClick={()=> setSideNavPress(false) }
-                  className='hover:bg-grey ease-in transition hover:cursor-pointer py-2 px-5' key={x}>
+                  className='hover:bg-grey ease-in transition hover:cursor-pointer py-2 pl-7' key={x}>
                     <Link href={`/list?type=${nav.type}&query=${category.filter}`}>
                       <p key={x} className=' font-medium text-base text-white whitespace-nowrap'>{category.name}</p>
                     </Link>
@@ -57,6 +62,7 @@ function SideNav({setSideNavPress, sideNavPress}:Props) {
               </div>
             </div>
           )}
+          </div>
         </div>
 
       </motion.div>
