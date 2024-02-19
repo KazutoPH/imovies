fb6b08dad21a0f712ca6055c87b854808aceea4d
KazutoPH <raymartgomez310@gmail.com>
2024-02-19 16:04:45 +0800

    Update page.tsx

--- app/list/page.tsx
+++ app/list/page.tsx
@@ -7,16 +7,14 @@ import { useSearchParams } from 'next/navigation'
 import { getMovies, searchMovie } from '@/lib/actions/movies.action'
 import React, { useEffect, useState } from 'react'
 import { MovieListSkeleton } from '@/components/skeleton'
+import Title from './title'
 
-function page() {
-
-  
+function page() {  
   return (
     <div className='flex flex-col items-center'>
       
       <div className='content-container'>
-      <Selection/>
-     
+      <Title/>
       <Movies/>
 
       </div>
