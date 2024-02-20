a7783bc4bf6f75bdddcba14f13a18edf3bb35efd
KazutoPH <raymartgomez310@gmail.com>
2024-02-20 16:47:31 +0800

    Create footer.tsx

---
+++ components/footer/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full flex items-center justify-center bg-yellow-400 py-1'>
+      <p className='text-base font-semibold text-dark'>iMovies 2024</p>
+    </div>
+  )
+}
+
+export default Footer
