fe506b52550d420ad4d32b0999961402e9393c50
KazutoPH <raymartgomez310@gmail.com>
2024-02-26 13:29:44 +0800

    Update modal.tsx

--- components/modal.tsx
+++ components/modal.tsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
 import { getTrailer } from '@/lib/actions/movies.action'
 import { FaX } from 'react-icons/fa6';
+import ErrorButton from './button/ErrorButton';
 
 interface trailerData {
   key?: string
@@ -20,16 +21,17 @@ function Modal() {
 
 
   useEffect(() => {
-    const fetchData = async () => {
+    if (search) {
       setShow(true)
-      const result = await getTrailer(search?.toString());
-      setTrailer(await result);
-    };
-
-    if (search)
       fetchData()
-
+    }
   }, [search])
+
+  const fetchData = async () => {
+    setTrailer(undefined)
+    const result = await getTrailer(search?.toString()).then(data => setTrailer(data));
+    console.log(trailer)
+  };
 
   // useEffect(()=> {
   //   async function getData(){
@@ -59,26 +61,45 @@ function Modal() {
   return (
     <>
       {show &&
-        <div className={` darkbg flex items-center justify-center flex-col z-[60] relative`} onClick={() => {
-          router.push(`${pathname}`)
-          setShow(false)
-          setTrailer(undefined)
-        }}>
+        <div className={` darkbg flex items-center justify-center flex-col z-[60] relative`}>
 
-          <div className=' h-auto w-[95%] aspect-video sm:w-4/5 p-2 bg-dark flex flex-col gap-1 relative'>
-          <div className='flex self-end hover:scale-110 active:scale-100 hover:cursor-pointer transition'
-        onClick={() => {
-          router.push(`${pathname}`)
-          setShow(false)
-          setTrailer(undefined)
-        }}>
-            <FaX
-            size={30}
-            color='white'/>
+          <div className=' h-auto w-[95%] aspect-video sm:w-4/5 p-2 bg-dark flex flex-col gap-1 relative z-[100]'>
+            <div className='flex self-end hover:scale-110 active:scale-100 hover:cursor-pointer transition'
+              onClick={() => {
+                router.push(`${pathname}`)
+                setShow(false)
+                setTrailer(undefined)
+              }}>
+              <FaX
+                size={30}
+                color='white' />
             </div>
-            {trailer &&
-              <iframe className='w-full aspect-video' src={`https://www.youtube.com/embed/${trailer}?&autoplay=1`} title="Movie Trailer" allow="autoplay; picture-in-picture;" allowFullScreen></iframe>
-            }
+
+            <div className=' w-full flex items-center justify-center h-full relative' >
+              {trailer ?
+                (
+                  <>
+                  {trailer === 'error' ? (
+                    <div className='flex flex-col items-center gap-3'>
+                      <p className='text-white text-base'>There was an error fetching data</p>
+                      <div onClick={()=> fetchData()}>
+                        <ErrorButton/>
+                      </div>
+
+                    </div>
+                  ):(
+                    <iframe className='w-full aspect-video' src={`https://www.youtube.com/embed/${trailer}?&autoplay=1`} title="Movie Trailer" allow="autoplay; picture-in-picture;" allowFullScreen></iframe>
+                  )}
+                  </>
+
+                ) : (
+                  <div className='relative'>
+                  <div className='videoloader' />
+                </div>
+                )
+              }
+            </div>
+
           </div>
         </div>
       }
