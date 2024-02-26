410743a1182e52a49066337d250c8b39e3b5eb48
KazutoPH <raymartgomez310@gmail.com>
2024-02-26 13:29:25 +0800

    Create ErrorButton.tsx

---
+++ components/button/ErrorButton.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ErrorButton() {
+  return (
+    <div className='bg-yellow-400 py-2 px-5 rounded-md text-dark font-semibold text-base hover:scale-110 active:scale-100 hover:cursor-pointer transition'>
+      <p> Try Again </p>
+    </div>
+  )
+}
+
+export default ErrorButton
